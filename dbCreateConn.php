3893130707a644a362c9ffc5c5e61194c87fe99f
anirudh55 <anirudh.ksg@gmail.com>
2015-07-02 12:31:03 +0200

    password

--- dbCreateConn.php
+++ dbCreateConn.php
@@ -1,8 +1,8 @@
 <?php 
 
 	$servername = "localhost";
-	$username = "anna";
-	$password = "karenina";
+	$username = "henk";
+	$password = "henk";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
